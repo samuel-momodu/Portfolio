abd529e14c2b5adfe7fa3cc26412f8e83791db3b
Samuel Momodu <shakafire73@gmail.com>
2021-09-20 16:27:12 +0100

    SQL Schemas

---
+++ OneDrive/Desktop/GIT Folders/Portfolio/SQL-Schemas/README.md
@@ -0,0 +1,34 @@
+# SQL SCHEMAS
+
+This codes above have been used to create tables on a database.
+
+## OVERVIEW
+
+It is vital to store data correctly. This ensure that it can be efficiently collected, transformed and loaded to the database with minimal memory usage.
+
+### DEFINING TABLE SCHEMAS
+
+The aim of this, is o create tables which clearly store data, without consuming lots of space.
+
+* Use primary keys to establish a relationships between rows and columns of a table.
+* Use foreign keeys to establish relationiships between one or more tables.
+* Set the data type for each row and column (either float, int or string).
+* Define the maximum number of characters that can exist in each row and column.
+
+### GOALS
+
+When a table schema is correctly defined in a relational database, it eradicates the possibility of data loss.
+This is very useful when setting up a relational database table for a business. This improves the accuracy of data stored in the realtional database.
+
+### BENEFITS
+
+* A proper schema makes it easy for the data to be stired directly from the point of trnasaction, into the database directly.
+* Data is readily acessible.
+* Data can be easily loaded into the database.
+* Data can be easily collected from the database.
+* Data can be easily analysed.
+* New data can be easily added.
+* Incorrect data can be easily corrected through updates.
+ 
+### QUERIES
+A few queries that show he created tables with utilizing the schema and sample datasets.
---
+++ OneDrive/Desktop/GIT Folders/Portfolio/SQL-Schemas/Student-Schemas-ID-Table.sql
@@ -0,0 +1,39 @@
+﻿/*# This query is used to create a sample schema for a table.
+# This table is used to record names, student identification number and majors of students.*/
+CREATE TABLE student (
+    student_id INT,
+    name VARCHAR(20),
+    major VARCHAR(20),
+    PRIMARY KEY(student_id)
+);
+SELECT * 
+FROM student
+WHERE major IN ('Biology', 'Chemistry') AND student_id >2;
+/*# We can use this to insert values into each column and row.*/
+INSERT INTO student (student_id, name, major) VALUES(1, 'Mori', 'UI/UX');
+INSERT INTO student (student_id, name, major) VALUES(2, 'Jack', 'Biology');
+INSERT INTO student (student_id, name, major) VALUES(3, 'Kate', 'Sociology');
+INSERT INTO student (student_id, name, major) VALUES(4, 'Claire', 'Chemistry');
+INSERT INTO student (student_id, name, major) VALUES(5, 'jack', 'Biology');
+INSERT INTO student (student_id, name, major) VALUES(6, 'Mike', 'Computer Science');
+
+/*#For times whe we have incomplete values, we can use this.*/
+INSERT INTO student(student_id, name) VALUES(3, 'Claire');
+
+/*# We can continuer to insert into the table.*/
+INSERT INTO student VALUES(4, 'Jack', 'Biology');
+INSERT INTO student VALUES(5, 'Mike', 'Computer Science');
+INSERT INTO student VALUES(6, 'Samuel', 'COmputer Science');
+/*# This is used to update or make corrections to rows and columns*/
+UPDATE student
+ SET major = 'Computer Science' 
+ WHERE student_id = 6;
+/*# We can also use update syntax to abbreviate data attributes in database tables*/
+ 
+UPDATE student 
+SET major = 'Comp Sci'
+WHERE major = 'Computer Science';
+
+UPDATE student 
+SET major = 'Biochemistry'
+WHERE major = 'Bio' OR major = 'Chemistry';
